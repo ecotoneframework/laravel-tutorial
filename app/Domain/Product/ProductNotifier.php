70a338a5f02082bfe75f94d2fc479c7772f76d1b
Dariusz Gafka <dgafka.mail@gmail.com>
2021-02-01 15:35:51 +0100

    Upgrade ecotone version

--- app/Domain/Product/ProductNotifier.php
+++ app/Domain/Product/ProductNotifier.php
@@ -1,12 +1,8 @@
 <?php
 namespace App\Domain\Product;
 
-use Ecotone\Messaging\Annotation\MessageEndpoint;
 use Ecotone\Modelling\Annotation\EventHandler;
 
-/**
- * @MessageEndpoint()
- */
 class ProductNotifier
 {
     /**

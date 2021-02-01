1d445168c6750aa31a373914553fa367d34837f6
Dariusz Gafka <dgafka.mail@gmail.com>
2021-02-01 16:32:42 +0100

    Add more descriptive version

--- app/EcotoneQuickstart.php
+++ app/EcotoneQuickstart.php
@@ -19,7 +19,23 @@ class EcotoneQuickstart
 
     public function run() : void
     {
+        $this->commandBus->convertAndSend(
+            "product.register",
+            MediaType::APPLICATION_X_PHP_ARRAY,
+            ["productId" => 1, "cost" => 100]
+        );
+        $this->commandBus->convertAndSend(
+            "product.register",
+            MediaType::APPLICATION_X_PHP_ARRAY,
+            ["productId" => 2, "cost" => 300]
+        );
+
         $orderId = 990;
+        $this->commandBus->convertAndSend(
+            "order.place",
+            MediaType::APPLICATION_X_PHP_ARRAY,
+            ["orderId" => $orderId, "productIds" => [1,2]]
+        );
 
         echo $this->queryBus->convertAndSend("order.getTotalPrice", MediaType::APPLICATION_X_PHP_ARRAY, ["orderId" => $orderId]);
     }

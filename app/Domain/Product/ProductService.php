3b25f6d8ba8d6f69d78182b2f2db8f2084629d52
dgafka <dgafka.mail@gmail.com>
2021-03-05 19:39:13 +0100

    Adjust to newest Ecotone

--- app/Domain/Product/ProductService.php
+++ app/Domain/Product/ProductService.php
@@ -2,27 +2,23 @@
 
 namespace App\Domain\Product;
 
-use Ecotone\Modelling\Annotation\CommandHandler;
-use Ecotone\Modelling\Annotation\QueryHandler;
+use Ecotone\Modelling\Attribute\CommandHandler;
+use Ecotone\Modelling\Attribute\QueryHandler;
 use Ecotone\Modelling\EventBus;
 
 class ProductService
 {
     private array $registeredProducts = [];
 
-    /**
-     * @CommandHandler()
-     */
+    #[CommandHandler]
     public function register(RegisterProductCommand $command, EventBus $eventBus) : void
     {
         $this->registeredProducts[$command->getProductId()] = $command->getCost();
 
-        $eventBus->send(new ProductWasRegisteredEvent($command->getProductId()));
+        $eventBus->publish(new ProductWasRegisteredEvent($command->getProductId()));
     }
 
-    /**
-     * @QueryHandler()
-     */
+    #[QueryHandler]
     public function getPrice(GetProductPriceQuery $query) : int
     {
         return $this->registeredProducts[$query->getProductId()];
